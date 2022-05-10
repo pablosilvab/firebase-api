7a8028aba91bb9843e3d15c32d294301ed7799c2
Pablo Silva <pablonicolassilvabravo@gmail.com>
2022-05-10 19:57:54 -0400

    add permissions

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,13 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin')
 const express = require('express')
+var serviceAccount = require("./config/serviceAccountKey.json");
 
 const app = express()
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 app.get('/hello', (req, res) => {
     return res.status(200).json({message: 'Hello World!'})

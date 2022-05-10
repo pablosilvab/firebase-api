2dbc5805a13cd81d8ca53b3efd0bb358a3b26966
Pablo Silva <pablonicolassilvabravo@gmail.com>
2022-05-10 19:52:29 -0400

    add get endpoint - hello world

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Firebase Rest API
 
 This project is a POC to use Firebase Functions as a backend in a project
+
+## Installation
+
+Run emulator locally
+
+```
+firebase emulators:start 
+```
+
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
 const functions = require("firebase-functions");
+const express = require('express')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express()
+
+app.get('/hello', (req, res) => {
+    return res.status(200).json({message: 'Hello World!'})
+})
+
+exports.app = functions.https.onRequest(app);

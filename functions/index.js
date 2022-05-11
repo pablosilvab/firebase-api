76683cfe7143ee163ebbae0791665ff285c93320
Pablo Silva <pablonicolassilvabravo@gmail.com>
2022-05-10 20:08:01 -0400

    add post endpoint

--- functions/index.js
+++ functions/index.js
@@ -6,11 +6,25 @@ var serviceAccount = require("./config/serviceAccountKey.json");
 const app = express()
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount)
 });
 
+const db = admin.firestore()
+
 app.get('/hello', (req, res) => {
-    return res.status(200).json({message: 'Hello World!'})
+    return res.status(200).json({ message: 'Hello World!' })
+})
+
+app.post('/api/books', async (req, res) => {
+    try {
+        await db.collection('books')
+            .doc('/' + req.body.id + '/')
+            .create({ name: req.body.name })
+        return res.status(204).json()
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send(err)
+    }
 })
 
 exports.app = functions.https.onRequest(app);
